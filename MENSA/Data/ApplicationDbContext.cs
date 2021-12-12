ca8320ace3bd430326720763912e59f91e5e6cc4
mvuko01 <mvuko01@fesb.hr>
2021-12-12 15:48:53 +0100

    Connected project to database (Implemented DbContext and Connection string), added Student table

---
+++ MENSA/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using MENSA.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MENSA.Data
+{
+    //DbContext nam sluzi za komunikaciju sa bazon i kako bi mogli slat podatke u bazu
+    // da bi koristili DbContext to moramo naslijediti od EntityFrameworkCore (using Microsoft.EntityFrameworkCore;)
+
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) //konstruktor
+        {
+
+        }
+
+        public DbSet<Student> Student { get; set; } //nadodajemo ovo kako bi mogli nas model ubaciti u bazu
+                                                    // da mos koristit model Student moras koristit using MENSA.Models;
+    }
+}
---
+++ MENSA/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MENSA.Models
+{
+    public class Student
+    {
+        [Key] //Key oznacava da je to ispod kljuc i da koristi identity svojstvo, treba ubacit using System.ComponentModel.DataAnnotations; da se moze koristit
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+    }
+}
