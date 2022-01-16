2e33dbd631311648b3eb89063f9808e93ac44e2d
mvuko01 <mvuko01@fesb.hr>
2022-01-16 19:53:53 +0100

    Added insert menza queries

---
+++ Queries/InsertMenza.sql
@@ -0,0 +1,6 @@
+INSERT INTO Menza
+         (Name, WorkingHours ,PicturePath)  
+VALUES   ('FESB-STOP','2001-01-10 10:40:50','fesb-donja.jpg'), ('FESB','2001-01-10 10:40:50','fesb-gornja.jpg'), 
+('Ekonomski Fakultet','2001-01-10 10:40:50','ekonomija.jpg'),('Kampus','2001-01-10 10:40:50','kampus.jpg'),
+('Index','2001-01-10 10:40:50','index.jpg'),('Spinut','2001-01-10 10:40:50','spinut.jpg'),('Medicinski Fakultet','2001-01-10 10:40:50','medicina.jpg')
+,('Gradevinski fakultet','2001-01-10 10:40:50','fgag.jpg')
