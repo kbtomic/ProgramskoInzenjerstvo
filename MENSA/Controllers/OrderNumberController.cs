722590ab1dd719dd5aaefba77d3f9d660d12b6bb
mvuko01 <mvuko01@fesb.hr>
2022-01-17 20:19:42 +0100

    Added order screen

---
+++ MENSA/Controllers/OrderNumberController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MENSA.Controllers
+{
+    public class OrderNumberController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
